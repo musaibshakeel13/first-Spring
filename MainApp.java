5b192284c4eb20806575b81af0758fed91ee8585
Musaib shakeel <hajimusaib0@gmail.com>
2026-09-10 22:20:01 +0530

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,1 +1,15 @@
 
+package com.example.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+    ApplicationContext context =
+            new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+    GreetService greatingService
+            = (GreetService) context.getBean("myBean");
+    greatingService.sayhello();
+    }
+}
